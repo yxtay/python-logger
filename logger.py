e2aae8b1c4151e1b5a954b635577f6461acbf2ac
yxtay <wyextay@gmail.com>
2017-07-13 10:53:16 +0800

    add null handler

--- logger.py
+++ logger.py
@@ -1,9 +1,9 @@
 import logging
 from logging.handlers import RotatingFileHandler
-from os import path
+import os
 
 
-def get_logger(name, log_path=path.join(path.dirname(__file__), "main.log"), console=False):
+def get_logger(name, log_path=os.path.join(os.path.dirname(__file__), "main.log"), console=False):
     """
     Simple logging wrapper that returns logger
     configured to log into file and console.
@@ -40,21 +40,25 @@ def get_logger(name, log_path=path.join(path.dirname(__file__), "main.log"), con
         ch.setFormatter(formatter)
         logger.addHandler(ch)
 
+    if len(logger.handlers) == 0:
+        logger.addHandler(logging.NullHandler())
+
     return logger
 
 
 if __name__ == "__main__":
     import argparse
-    import os
     parser = argparse.ArgumentParser(description="Simple script to run `get_logger()`.")
-    parser.add_argument("--path", help="path of log file", default="log")
-    parser.add_argument("--reset", help="whether to reset log file", action="store_true")
+    parser.add_argument("--log-path", default="main.log",
+                        help="path of log file")
+    parser.add_argument("--reset", action="store_true",
+                        help="whether to reset log file")
     args = parser.parse_args()
 
-    if args.reset and os.path.exists(args.path):
-        os.remove(args.path)
+    if args.reset and os.path.exists(args.log_path):
+        os.remove(args.log_path)
 
-    logger = get_logger(__name__, args.path, True)
+    logger = get_logger(__name__, args.log_path, True)
     logger.debug("This is a debug message.")
     logger.info("This is an info message.")
     logger.warning("This is a warning message.")
@@ -65,5 +69,14 @@ if __name__ == "__main__":
     except ValueError as e:
         logger.exception(e)
 
-    logger = get_logger(__name__, args.path, True)
+    logger = get_logger(__name__, args.log_path, True)
     logger.info("This message should appear just once.")
+
+    logger = get_logger(__name__, "", True)
+    logger.info("This message should appear in the console only.")
+
+    logger = get_logger(__name__, args.log_path, False)
+    logger.info("This message should appear in the log file only.")
+
+    logger = get_logger(__name__, "", False)
+    logger.info("This message should not appear.")
